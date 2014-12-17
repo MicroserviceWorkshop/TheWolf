6c09e481f830d8ff3abaabdd953b566288167047
Roman Schaller <roman.schaller@gmail.com>
2014-12-17 19:41:15 +0100

    Register at ZooKeeper at startup

--- sales/src/main/java/thewolf/sales/Application.java
+++ sales/src/main/java/thewolf/sales/Application.java
@@ -2,12 +2,19 @@ package thewolf.sales;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-	
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  @Bean
+  public ApplicationListener<EmbeddedServletContainerInitializedEvent> abc() {
+    return new ZooInitializer();
+  }
 }
---
+++ sales/src/main/java/thewolf/sales/ZooInitializer.java
@@ -0,0 +1,42 @@
+package thewolf.sales;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.RetryNTimes;
+import org.apache.curator.x.discovery.ServiceDiscoveryBuilder;
+import org.apache.curator.x.discovery.ServiceInstance;
+import org.apache.curator.x.discovery.UriSpec;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent;
+import org.springframework.context.ApplicationListener;
+
+
+/**
+ * This initializer registers itself at a ZooKeeper instance at localhost on Port 2181.
+ * 
+ * @author roman
+ *
+ */
+public class ZooInitializer implements ApplicationListener<EmbeddedServletContainerInitializedEvent> {
+
+  @Override
+  public void onApplicationEvent(EmbeddedServletContainerInitializedEvent event) {
+    int localPort = event.getEmbeddedServletContainer().getPort();
+    registerAtZooKeeper(localPort, "localhost:2181", "sales");
+  }
+
+  private void registerAtZooKeeper(int localPort, String zookeeperHostAndPort, String applicationName) {
+    CuratorFramework curatorFramework = CuratorFrameworkFactory.newClient(zookeeperHostAndPort, new RetryNTimes(5, 1000));
+    curatorFramework.start();
+    ServiceInstance<Object> serviceInstance;
+    try {
+      serviceInstance =
+          ServiceInstance.builder().uriSpec(new UriSpec("{scheme}://{address}:{port}/")).address("localhost").port(localPort)
+              .name(applicationName).build();
+      ServiceDiscoveryBuilder.builder(Object.class).basePath("thewolf").client(curatorFramework).thisInstance(serviceInstance).build()
+          .start();
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+
+  }
+}
