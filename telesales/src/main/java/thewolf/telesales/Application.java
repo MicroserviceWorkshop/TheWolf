339b980e5d9e2086d37e3fe19c760c282e479302
PoliM <marco.a.poli@gmail.com>
2014-12-18 15:45:28 +0100

    Add Hystrix

--- telesales/src/main/java/thewolf/telesales/Application.java
+++ telesales/src/main/java/thewolf/telesales/Application.java
@@ -2,12 +2,14 @@ package thewolf.telesales;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
+@EnableHystrix
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-	
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
 }
