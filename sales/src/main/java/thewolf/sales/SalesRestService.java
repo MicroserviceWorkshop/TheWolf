bdcb6d692708e93c959f76f42c1690524d007989
Roman Schaller <roman.schaller@gmail.com>
2014-12-18 22:58:51 +0100

    Change from JAX-RS to Spring annotations

--- sales/src/main/java/thewolf/sales/SalesRestService.java
+++ sales/src/main/java/thewolf/sales/SalesRestService.java
@@ -3,47 +3,45 @@ package thewolf.sales;
 import java.net.URI;
 
 import javax.inject.Inject;
-import javax.inject.Named;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
-import javax.ws.rs.core.UriBuilder;
-import javax.ws.rs.core.UriInfo;
 
-@Named
-@Path("/salesorders")
-@Produces({MediaType.APPLICATION_JSON})
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Controller
+@RequestMapping("/salesorders")
 public class SalesRestService {
 
   @Inject
   private SalesService salesService;
-  @Context
-  private UriInfo uriInfo;
 
-  @GET
-  @Path("/{id}")
-  public Response get(@PathParam("id") int id) {
+  @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+  public ResponseEntity<SalesOrder> get(@PathVariable int id) {
     SalesOrder salesOrder = salesService.getOrder(id);
     if (salesOrder != null) {
-      return Response.ok(salesOrder).build();
+      return new ResponseEntity<SalesOrder>(salesOrder, HttpStatus.OK);
     } else {
-      return Response.status(Status.NOT_FOUND).build();
+      return new ResponseEntity<SalesOrder>(HttpStatus.NOT_FOUND);
     }
   }
 
-  @POST
-  @Consumes(MediaType.APPLICATION_JSON)
-  public Response post(SalesOrder salesOrder) {
-    SalesOrder addedOrder = salesService.addOrder(salesOrder);
-    UriBuilder fromResource = uriInfo.getBaseUriBuilder().path(getClass()).path(getClass(), "get");
-    URI location = fromResource.build(addedOrder.getId());
-    return Response.created(location).build();
+  @RequestMapping(method = RequestMethod.POST)
+  public ResponseEntity<?> post(UriComponentsBuilder cmpBuilder, @RequestBody SalesOrder salesOrder) {
+    try {
+      SalesOrder addedOrder = salesService.addOrder(salesOrder);
+      URI location = cmpBuilder.path("/salesorders/{id}").build().expand(addedOrder.getId()).toUri();
+      HttpHeaders headers = new HttpHeaders();
+      headers.setLocation(location);
+      return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+    } catch (Exception e) {
+      e.printStackTrace();
+      return new ResponseEntity<Void>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
